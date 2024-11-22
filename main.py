c713672a85b61b050053d71fcec16dbd4f48a3e0
Danil <danig1991@mail.ru>
2024-11-22 15:43:52 +0300

    ДЗ: Создание класса и метода

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-# базовый url
-base_url = 'https://www.saucedemo.com/'
-
-# добавить опции/оставить браузер открытым
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-# автоматическая загрузка драйвера
-service = ChromeService(ChromeDriverManager().install())
-
 
 class Test():
     def __init__(self):
         self.driver_chrome = None
 
     def open_browser(self):
+        # базовый url
+        base_url = 'https://www.saucedemo.com/'
+
+        # добавить опции/оставить браузер открытым
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        # автоматическая загрузка драйвера
+        service = ChromeService(ChromeDriverManager().install())
+
         # открытие браузера с параметрами
         self.driver_chrome = webdriver.Chrome(
             options=options,
